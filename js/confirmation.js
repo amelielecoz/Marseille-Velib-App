d90d2b7d50f0334ff3e95b740c5b68c21d854d75
Le Coz <lecozamelie@hotmail.com>
2019-03-01 09:36:18 +0100

    modification timer

--- js/confirmation.js
+++ js/confirmation.js
@@ -4,11 +4,13 @@ class Confirmation {
         this.timerId = timerId;
         this.confirmation = document.getElementById(this.element);
         this.interval;
-        this.cancelElt = cancelElt
+        this.cancelElt = cancelElt;
         this.countdown();
         this.confirmBooking();
-        this.cancelBooking(cancelElt)
-
+        this.cancelBooking(cancelElt);
+        let timer = "20:00";
+        document.getElementById(this.timerId).textContent = timer;
+        
     }
 
     /**
@@ -16,8 +18,7 @@ class Confirmation {
      */
     countdown() {
         // clearInterval(interval);
-        let interval = setInterval( () =>{
-            let timer = document.getElementById(this.timerId).innerHTML;
+        setInterval( () => {
             timer = timer.split(':');
             let minutes = timer[0];
             let seconds = timer[1];
@@ -31,36 +32,36 @@ class Confirmation {
                 seconds = '0' + seconds;
                 timer = (minutes + ':' + seconds);
             }
-                  
-            if (minutes == 0 && seconds == 0) {
-              clearInterval(interval);
-              //annulation();
+            localStorage.setItem("timer", timer);  
+
+            if (minutes === 0 && seconds === 0) {
+                clearInterval(interval);
+                localStorage.removeItem('timer')
+                //annulation();
             }
         }, 1000);
-      }
+    }
 
     confirmBooking() {
-        Canvas.saveButton.addEventListener('click', function(){
-            var address = localStorage.getItem("adresse");
-            document.getElementById("adresse-validee").innerHTML = '<i class="fas fa-map-marker-alt"></i> Adresse : ' + address;
-            document.getElementById('timer').textContent = "20:00";
-            countdown(); 
-            setInterval(countdown(), 1000); //Déclenchement du compte à rebours
-            this.confirmation.style.display = "block";
-            signaturePad.style.visibility ="hidden";
-            signaturePad.style.height = "0px"
-            let canvasImg = localStorage.getItem('canvas');
-            document.getElementById('canvas-img').innerHTML = canvasImg;
-        });
+        var address = localStorage.getItem("adresse");
+        document.getElementById("adresse-validee").innerHTML = address;
+        this.countdown();
+        setInterval(this.countdown(), 1000); //Déclenchement du compte à rebours
+        this.confirmation.style.display = "block";
+        let signaturePad = document.getElementById("signature-pad")
+        signaturePad.style.visibility ="hidden";
+        signaturePad.style.height = "0px"
+        let canvasImg = localStorage.getItem('canvas');
+        document.getElementById('canvas-img').innerHTML = canvasImg;
+        
     }
 
     cancelBooking(cancelElt) {
         let btnAnnuler = document.getElementById(cancelElt);
-
         btnAnnuler.addEventListener('click', () => {
             this.confirmation.style.display = "none";
             document.getElementById("h1-reservation").innerHTML = "Réservation annulée";
             document.getElementById("h1-details").innerHTML = "";
         });
     }
-}
+} 
