aa197d2e1e07310d66899114a11c19b7f24d7dc3
Le Coz <lecozamelie@hotmail.com>
2019-03-12 10:00:18 +0100

    vérif comments + ajout initCanvas()

--- js/canvas.js
+++ js/canvas.js
@@ -3,99 +3,73 @@ class Canvas {
         this.element = element; 
         this.saveElt = saveElt; 
         this.clearElt = clearElt; 
+        this.initCanvas();
+    }
 
-        let canvas = document.querySelector(element);
+    /**
+     * @description initialise le canvas
+     */
+    initCanvas() {
+        let canvas = document.querySelector(this.element);
         this.ctx = canvas.getContext("2d");
         this.saveButton = document.getElementById(this.saveElt);
         this.clearButton = document.getElementById(this.clearElt);
-    
         canvas.width = canvas.parentNode.clientWidth;
         canvas.maxWidth = 300;
         canvas.height = 150;
-    
         this.ctx.strokeStyle = "#000";
         this.ctx.lineJoin = "round";
         this.ctx.lineCap = "round";
         this.ctx.lineWidth = 2;
         this.ctx.fillStyle = "#fff";
         this.ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
         this.isDrawing = false;
-        this.drawEmpty = true;
+        this.drawEmpty;
         this.lastX = 0;
         this.lastY = 0;
-
-        
         this.initDraw();
         this.effaceSignature();
         this.enregistreSignature();
-
     }
 
-
     /**
-     * Dessine
+     * @description Dessine
+     * @param {number} x coordonnées du dessin
+     * @param {number} y coordonnées du dessin
      */
     draw(x, y) {
+        this.drawEmpty = true;
         if (!this.isDrawing) 
-            return; // stop the fn from running when they are not moused down
+            return; 
         this.ctx.beginPath();
-        // start from
         this.ctx.moveTo(this.lastX, this.lastY);
-        // go to
         this.ctx.lineTo(x, y);
         this.ctx.stroke();
         [this.lastX, this.lastY] = [x, y];
         this.drawEmpty = false;
     }
 
-    
-    //Initialise le dessin
+    /**
+     * @description Actualise le dessin en fonction de l'évenement passé par la souris
+     */
     initDraw() {
-        
         canvas.addEventListener("mousedown", e => {
             this.isDrawing = true;
             [this.lastX, this.lastY] = [e.offsetX, e.offsetY];
         });
-        canvas.addEventListener("touchstart", e => {
-            if (e.touches && e.touches.length == 1) {
-            this.isDrawing = true;
-            let touch = e.touches[0];
-            let touchX = touch.pageX;
-            let touchY = touch.pageY;
-            [this.lastX, this.lastY] = [touchX, touchY];
-            e.preventDefault();
-            }
-        });
-    
-        //Dessine
         canvas.addEventListener("mousemove", (e) => {
             this.draw(e.offsetX, e.offsetY);
         });
-        canvas.addEventListener("touchmove", (e) => {
-            if (e.touches && e.touches.length === 1) {
-            let touch = e.touches[0];
-            let touchX = touch.pageX;
-            let touchY = touch.pageY;
-            this.draw(touchX, touchY);
-            }
-        });
-        
-        //Termine le dessin
         canvas.addEventListener("mouseup", () => {
             this.isDrawing = false;
         });
         canvas.addEventListener('mouseout', () => {
             this.isDrawing = false;
         });
-        canvas.addEventListener("touchend", () => {
-            this.isDrawing = false;
-        });
     }
 
-    
     /**
-     * Fonction pour effacer la signature
+     * @description Efface le contenu du canvas
      */
     effaceSignature() {
         this.clearButton.addEventListener("click", ()=> {
@@ -104,7 +78,9 @@ class Canvas {
         });
     }
     
-    
+    /**
+     * @description Enregistre le contenu du canvas s'il n'est pas vide, ou envoie un message d'alerte s'il est vide
+     */
     enregistreSignature() {
         this.saveButton.addEventListener("click", () => {
             if (this.drawEmpty !== true ) {
@@ -112,23 +88,20 @@ class Canvas {
                 let canvasImage = document.getElementById("canvas-img");
                 canvasImage.src = dataCanvas;
                 canvasImage.style.display = "block";
-                
                 document.getElementById("alert-signature").style.display = "none";
                 document.getElementById("h1-reservation").style.display = "block";
                 document.getElementById("h1-reservation").innerHTML = "Réservation confirmée";
                 document.getElementById("h1-details").style.display = "block";
                 document.getElementById("h1-details").innerHTML = prenom.value.toUpperCase() + ", votre réservation est valable pour 20 minutes.";    
-                let confirmation = new Confirmation("confirmation", "timer", "annuler");
+                new Confirmation("confirmation", "timer", "annuler");
                 localStorage.setItem("canvas", dataCanvas);
             } else {
                 document.getElementById("alert-signature").style.display = "block";
                 document.getElementById("h1-reservation").style.display = "none";
                 document.getElementById("h1-details").style.display = "none";
             }
-        });
-        
+        });  
     }
-
 }
 
 
